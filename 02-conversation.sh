7c0d92ab50bba550fe3f848275b1eb06a4faf6dd
raman <ramanj786@gmail.com>
2026-04-28 08:48:04 +0530

    first updaated commit and hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Ramesh:: Hi Suresh, How are you ?
+#Suresh:: Hello Ramesh, I am fine. How are you ?
+#Ramesh:: what are you learning recently ?
+#Suresh:: I am learning shell scripting Ramehs
+
+# If you want replace Ramesh name with John and Suresh name with Raheem,, for that need to use variables like below.
+
+# No space before or after equal
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: $PERSON2, How are you ?
+echo "$PERSON2:: $PERSON1, I am fine. How are you ?
+echo "$PERSON1:: what are you learning recently ?
+echo "$PERSON2:: I am learning shell scripting $PERSON1
